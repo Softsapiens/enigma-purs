ebaa44d1f2d9cf66b5d6e608da709dd1c88fadd6
dani <dani@softsapiens.com>
2016-02-28 22:48:08 +0100

    fx -> loop

--- src/Main.purs
+++ src/Main.purs
@@ -1,12 +1,12 @@
 module Main where
 
-import Prelude (Unit, unit, ($), class Ord, class Show, show, class Semiring, add, zero, mul
-                , (++), (-), bind, (>=), otherwise)
+import Prelude (Unit, unit, ($), class Ord, show, class Semiring, add, zero, mul
+                , (++), (-), (>=), otherwise)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Data.Tuple (Tuple(Tuple), fst, snd)
-import Data.List (List(Nil, Cons), toList, take, drop)
-import Data.Map (Map, fromList, lookup, keys)
+import Data.List (List(Nil, Cons), take, drop)
+import Data.Map (Map, lookup, keys)
 import Data.Maybe (Maybe(Just, Nothing))
 import Unsafe.Coerce (unsafeCoerce)
 import Data.Foldable (class Foldable, foldl)
@@ -14,65 +14,49 @@ import Data.Foldable (class Foldable, foldl)
 todo :: forall a. a
 todo = unsafeCoerce unit
 
-message :: Message Int
-message = toList [1, 2, 3, 2, 3, 4, 3, 2, 3, 2, 3, 4, 4, 8]
-
-dictionary :: Dictionary Int String
-dictionary = fromList $ toList [ (Tuple 23 "N"), Tuple 234 "T", Tuple 8 "Q" ]
-
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
-  log $ "Message= " ++ show message
-  log $ "Dictionary= " ++ show dictionary
-  newline
-  log $ "decipher= " ++ decipher dictionary message
-  where
-  newline = log " "
+  log "Run tests: 'pulp test'"
 
 type Message k = List k
 type Dictionary k v = Map k v
 
+toNumber :: forall f k. (Foldable f, Semiring k) => k -> f k -> k
+toNumber b xs = foldl (\x y -> add (mul x b) y) zero xs
+
 -- TODO: abstract over String and Int
---decipher :: forall k. (Ord k, Show k, Semiring k) => Dictionary k String -> Message k -> String
 decipher :: Dictionary Int String -> Message Int -> String
 decipher d m = snd t
   where
     max :: forall a. (Ord a) => a -> a -> a
     max x y | x >= y    = x
             | otherwise = y
-    maxDg = foldl max 0 $ keys d
-    t = fx d m maxDg ""
 
-toNumber :: forall f a. (Foldable f, Semiring a) => a -> f a -> a
-toNumber b xs = foldl (\x y -> add (mul x b) y) zero xs
+    maxDg = foldl max zero $ keys d
+    t = loop d toKey m maxDg ""
+    toKey = (toNumber 10)
 
 -- TODO: abstract over String and Int
--- TODO: search in dictionary
--- Search cs list options in dict
--- returns the finded code as a pair (length key, value)
-findCode :: Dictionary Int String -> Message Int -> Tuple Int String
-findCode d Nil = Tuple 1 "ERROR"
-findCode d cs =
-  case lookup (toNumber 10 cs) d of
+findCode :: Dictionary Int String -> (Message Int -> Int) -> Message Int -> Tuple Int String
+findCode d toKey cs =
+  case lookup (toKey cs) d of
     Just x -> Tuple l x
     Nothing -> case cs of
                 Cons x Nil -> Tuple 1 $ show x
-                _ -> findCode d $ take (l- 1) cs
+                _ -> findCode d toKey $ take (l - 1) cs
   where
     l = Data.List.length cs
--- d dict
--- m current msg
--- take tn
--- accumulator String
+
 -- TODO: abstract over String and Int
-fx :: Dictionary Int String -> Message Int -> Int -> String -> Tuple Int String
-fx d m tn acc =
+-- TODO: abstract return type is a monoid
+loop :: Dictionary Int String -> (Message Int -> Int) -> Message Int -> Int -> String -> Tuple Int String
+loop d toKey m tn acc =
   case mpen of
     Nil -> Tuple np (acc ++ code)
-    _ -> fx d mpen tn (acc ++ code)
+    _ -> loop d toKey mpen tn (acc ++ code)
   where
     es = take tn m
-    t = findCode d es
+    t = findCode d toKey es
     np = fst t
     code = snd t
     mpen = drop np m
--- test/Main.purs
+++ test/Main.purs
@@ -6,13 +6,14 @@ import Control.Monad.Eff.Console (CONSOLE, log)
 import Data.Tuple (Tuple(Tuple))
 import Data.List (toList)
 import Data.Map (fromList)
-import Main (Message, Dictionary, decipher, message, dictionary)
+import Main (Message, Dictionary, decipher)
 
-msgTest :: Message Int
-msgTest = toList [1, 2, 3, 2, 3, 4]
 
-dictTest :: Dictionary Int String
-dictTest = fromList $ toList [ (Tuple 23 "N"), Tuple 234 "T" ]
+message :: Message Int
+message = toList [1, 2, 3, 2, 3, 4, 3, 2, 3, 2, 3, 4, 4, 8]
+
+dictionary :: Dictionary Int String
+dictionary = fromList $ toList [ (Tuple 23 "N"), Tuple 234 "T", Tuple 8 "Q" ]
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
