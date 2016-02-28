770adff0efa048af2acded45539b420afcc29c5a
dani <dani@softsapiens.com>
2016-02-28 22:12:54 +0100

    toNumber implemented.

--- src/Main.purs
+++ src/Main.purs
@@ -1,6 +1,6 @@
 module Main where
 
-import Prelude (Unit, unit, ($), (+), class Ord, class Show, show, class Semiring, add, zero
+import Prelude (Unit, unit, ($), class Ord, class Show, show, class Semiring, add, zero, mul
                 , (++), (-), bind, (>=), otherwise)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
@@ -15,16 +15,16 @@ todo :: forall a. a
 todo = unsafeCoerce unit
 
 message :: Message Int
-message = toList [1, 2, 3, 2, 3, 4, 3, 2, 3, 2, 3, 4]
+message = toList [1, 2, 3, 2, 3, 4, 3, 2, 3, 2, 3, 4, 4, 8]
 
 dictionary :: Dictionary Int String
-dictionary = fromList $ toList [ (Tuple 23 "N"), Tuple 234 "T" ]
+dictionary = fromList $ toList [ (Tuple 23 "N"), Tuple 234 "T", Tuple 8 "Q" ]
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
   log $ "Message= " ++ show message
   log $ "Dictionary= " ++ show dictionary
-  log $ "findCode= " ++ snd (findCode dictionary (toList [2, 3, 4]))
+  newline
   log $ "decipher= " ++ decipher dictionary message
   where
   newline = log " "
@@ -43,22 +43,23 @@ decipher d m = snd t
     maxDg = foldl max 0 $ keys d
     t = fx d m maxDg ""
 
-toNumber :: forall f a. (Foldable f, Semiring a) => f a -> a
-toNumber xs = foldl add zero xs
+toNumber :: forall f a. (Foldable f, Semiring a) => a -> f a -> a
+toNumber b xs = foldl (\x y -> add (mul x b) y) zero xs
 
 -- TODO: abstract over String and Int
 -- TODO: search in dictionary
 -- Search cs list options in dict
 -- returns the finded code as a pair (length key, value)
 findCode :: Dictionary Int String -> Message Int -> Tuple Int String
+findCode d Nil = Tuple 1 "ERROR"
 findCode d cs =
-  case cs of
-    Cons x Nil -> Tuple 1 $ show x
-    Cons 2 (Cons 3 (Cons 4 Nil)) -> Tuple 3 "T"
-    Cons 2 (Cons 3 Nil) -> Tuple 2 "N"
-    Nil -> Tuple 0 ""
-    _ -> findCode d $ take (Data.List.length cs - 1) cs
-
+  case lookup (toNumber 10 cs) d of
+    Just x -> Tuple l x
+    Nothing -> case cs of
+                Cons x Nil -> Tuple 1 $ show x
+                _ -> findCode d $ take (l- 1) cs
+  where
+    l = Data.List.length cs
 -- d dict
 -- m current msg
 -- take tn
--- test/Main.purs
+++ test/Main.purs
@@ -1,15 +1,12 @@
 module Test.Main where
 
-import Prelude (Unit, unit, ($), (+), class Ord, bind, show, (++))
+import Prelude (Unit, ($), bind, show, (++))
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Data.Tuple (Tuple(Tuple), fst, snd)
-import Data.List (List(Nil, Cons), toList)
-import Data.Map (Map, fromList, lookup)
-import Data.Maybe (Maybe(Just, Nothing))
-import Data.Foldable (class Foldable, foldl)
-
-import Main (Message, Dictionary, decipher, findCode, fx, message, dictionary)
+import Data.Tuple (Tuple(Tuple))
+import Data.List (toList)
+import Data.Map (fromList)
+import Main (Message, Dictionary, decipher, message, dictionary)
 
 msgTest :: Message Int
 msgTest = toList [1, 2, 3, 2, 3, 4]
@@ -24,12 +21,7 @@ main = do
   log $ "Message= " ++ show message
   log $ "Dictionary= " ++ show dictionary
   newline
-  log $ "findCode= " ++ snd (findCode dictionary (toList [2, 3, 4]))
-  log $ "fx= " ++ show (fx dictionary (toList [1, 2, 3, 2, 3, 4, 3, 2, 3, 2, 3, 4]) 3 "")
-  newline
   log $ "decipher= " ++ decipher dictionary message
   newline
-  log $ decipher d $ toList [1]
-  log $ decipher d $ toList [2, 3]
   where
   newline = log " "
